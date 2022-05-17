fac3cfacd9f9509fe4596a35ae81c33c6ba79a63
wangrui <wangrui@oecore.com>
2022-05-17 12:27:42 +0800

    1. support null safety.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String deviceName;
-  double screenSizeInches;
-  DeviceClassType deviceClass;
+  String? deviceName;
+  double? screenSizeInches;
+  DeviceClassType? deviceClass;
 
   @override
   void initState() {
